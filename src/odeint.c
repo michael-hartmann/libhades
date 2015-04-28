1b32a306e93a4b376b29ad7b6703dd7f1d91d5e4
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-04-28 12:40:06 +0200

    wip

--- src/odeint.c
+++ src/odeint.c
@@ -27,21 +27,84 @@
  *  @{
  */
 
-/* d/dt y = f(t,y), y(0)=y0 */
-#define RK4(FUNCTION_NAME, TYPE, MATRIX_TYPE, MATRIX_ALLOC, MATRIX_ADD, MATRIX_FREE) \
-int FUNCTION_NAME(void (*f)(double t, MATRIX_TYPE *y, MATRIX_TYPE *ft, void *args), MATRIX_TYPE *y0, double t0, double t, double h, void *args) \
+#define RK4_INIT(FUNCTION_NAME, RK4_TYPE, RK4_FREE, MATRIX_TYPE, MATRIX_ALLOC) \
+    int FUNCTION_NAME(RK4_TYPE *self, void (*f)(MATRIX_TYPE *, MATRIX_TYPE *, double, void *), int dim, void *args) \
+    { \
+        self->f    = f; \
+        self->args = args; \
+\
+        for(int i = 0; i < sizeof(self->workspace)/sizeof(self->workspace[0]); i++) \
+            self->workspace[i] = NULL; \
+\
+        for(int i = 0; i < sizeof(self->workspace)/sizeof(self->workspace[0]); i++) \
+        { \
+            self->workspace[i] = MATRIX_ALLOC(dim,1); \
+            if(self->workspace[i] == NULL) \
+            { \
+                RK4_FREE(self); \
+                return LIBHADES_ERROR_OOM; \
+            } \
+        } \
+\
+        return 0; \
+    }
+
+
+/** @brief Initialize workspace for explicit 4th order Runge Kutta method
+ *
+ * This function will initialize the Runge Kutta 4th order integration to solve
+ * the differential equation
+ *      d/dt y = f(y,t),
+ * where y is a vector and f is a arbitrary function for initial condition
+ * y(t0) = y0. The differential equation is solved using explicit Runge Kutta
+ * 4th order.
+ *
+ * The callback function f must have type
+ *      (*f)(matrix_complex_t *dest, matrix_complex_t *src, double t, void *args).
+ * The result must be stored in dest, the input vector is src, the time is t,
+ * and args is an arbitrary pointer given to the function.
+ *
+ * @param [in,out] self explicit Runge-Kutta 4th order object for real differential equation
+ * @param [in] f callback for rhs of differential equation
+ * @param [in] dim dimension of differential equation (rows of vector y)
+ * @param [in] args arbitrary pointer that will be passed to f
+ *
+ * @return ret 0 if successfull, otherwise LIBHADES_ERROR_OOM
+ */
+RK4_INIT(rk4_init, rk4_t, rk4_free, matrix_t, matrix_alloc)
+
+
+/** @brief Initialize workspace for explicit 4th order Runge Kutta method
+ *
+ * See rk4_init
+ */
+RK4_INIT(rk4_complex_init, rk4_complex_t, rk4_complex_free, matrix_complex_t, matrix_complex_alloc)
+
+#define RK4_FREE(FUNCTION_NAME, RK4_TYPE, MATRIX_FREE) \
+void FUNCTION_NAME(RK4_TYPE *self) \
+{ \
+    for(int i = 0; i < 5; i++) \
+        if(self->workspace[i] != NULL) \
+            MATRIX_FREE(self->workspace[i]); \
+}
+
+RK4_FREE(rk4_free, rk4_t, matrix_free)
+RK4_FREE(rk4_complex_free, rk4_complex_t, matrix_complex_free)
+
+
+#define RK4_INTEGRATE(FUNCTION_NAME, RK4_TYPE, TYPE, MATRIX_TYPE, MATRIX_ALLOC, MATRIX_ADD, MATRIX_FREE) \
+int FUNCTION_NAME(RK4_TYPE *self, MATRIX_TYPE *y0, double t, double t0, int steps) \
 { \
     const int N = y0->rows; \
-    int ret = 0; \
     double tn = t0; \
-    const int steps = ceil((t-t0)/h); \
-    h = t/steps; \
+    const double h = (t-t0)/steps;\
     MATRIX_TYPE *yn = y0; \
 \
-    MATRIX_TYPE *k1 = MATRIX_ALLOC(N,1); \
-    MATRIX_TYPE *k2 = MATRIX_ALLOC(N,1); \
-    MATRIX_TYPE *k3 = MATRIX_ALLOC(N,1); \
-    MATRIX_TYPE *k4 = MATRIX_ALLOC(N,1); \
+    MATRIX_TYPE *k1 = self->workspace[0]; \
+    MATRIX_TYPE *k2 = self->workspace[1]; \
+    MATRIX_TYPE *k3 = self->workspace[2]; \
+    MATRIX_TYPE *k4 = self->workspace[3]; \
+    MATRIX_TYPE *z  = self->workspace[4]; \
 \
     TYPE *ynM = yn->M; \
     TYPE *k1M = k1->M; \
@@ -49,46 +112,32 @@ int FUNCTION_NAME(void (*f)(double t, MATRIX_TYPE *y, MATRIX_TYPE *ft, void *arg
     TYPE *k3M = k3->M; \
     TYPE *k4M = k4->M; \
 \
-    MATRIX_TYPE *z = MATRIX_ALLOC(N,1); \
-\
-    if(k1 == NULL || k2 == NULL || k3 == NULL || k4 == NULL || z == NULL)\
-    { \
-        ret = LIBHADES_ERROR_OOM; \
-        goto out; \
-    } \
-\
     for(int n = 0; n < steps; n++) \
     { \
         tn = t0+n*h; \
 \
         /* k1 */ \
-        f(tn+h,   yn, k1, args); \
+        self->f(k1, yn, tn+h, self->args); \
 \
         /* k2 */ \
         MATRIX_ADD(yn, k1, h/2, z); \
-        f(tn+h/2, z, k2, args); \
+        self->f(k2, z, tn+h/2, self->args); \
 \
         /* k3 */ \
         MATRIX_ADD(yn, k2, h/2, z); \
-        f(tn+h/2, z, k3, args); \
+        self->f(k3, z, tn+h/2, self->args); \
 \
         /* k4 */ \
         MATRIX_ADD(yn, k3, h, z); \
-        f(tn+h,   z, k4, args); \
+        self->f(k4, z, tn+h, self->args); \
 \
         for(int j = 0; j < N; j++) \
             ynM[j] += h/6*(k1M[j]+2*(k2M[j]+k3M[j])+k4M[j]); \
     } \
- \
-    out: \
-    MATRIX_FREE(k1); \
-    MATRIX_FREE(k2); \
-    MATRIX_FREE(k3); \
-    MATRIX_FREE(k4); \
-    MATRIX_FREE(z); \
 \
-    return ret; \
+    return 0; \
 } \
+
 
 /** @brief Ruge Kutta 4th order for real ODE
  *
@@ -108,14 +157,42 @@ int FUNCTION_NAME(void (*f)(double t, MATRIX_TYPE *y, MATRIX_TYPE *ft, void *arg
  * @param [in] h precision
  * @param [in] args pointer given to function f
  */
-RK4(rk4, double, matrix_t, matrix_alloc, matrix_add, matrix_free);
+RK4_INTEGRATE(rk4_integrate, rk4_t, double, matrix_t, matrix_alloc, matrix_add, matrix_free);
+
 
 /** @brief Ruge Kutta 4th order for complex ODE
  *
  * See rk4.
  */
-RK4(rk4_complex, complex_t, matrix_complex_t, matrix_complex_alloc, matrix_complex_add, matrix_complex_free);
+RK4_INTEGRATE(rk4_complex_integrate, rk4_complex_t, complex_t, matrix_complex_t, matrix_complex_alloc, matrix_complex_add, matrix_complex_free);
 
+
+#define RK4_INTEGRATE_VARIABLE_STEPSIZE(FUNCTION_NAME, RK4_TYPE, RK4_INTEGRATE_FUNC, MATRIX_TYPE) \
+    int FUNCTION_NAME(RK4_TYPE *self, MATRIX_TYPE *yn, double t, double t0, double (*adapt)(void (*f)(MATRIX_TYPE *, MATRIX_TYPE *, double, void *), MATRIX_TYPE *y, double t, void *args)) \
+    { \
+        int done = 0, steps = 0; \
+        double tn = t0; \
+\
+        while(!done) \
+        { \
+            double h = adapt(self->f, yn, tn, self->args); \
+            if((tn+h) > t) \
+            { \
+                h = t-tn; \
+                done = 1; \
+            } \
+\
+            RK4_INTEGRATE_FUNC(self, yn, tn+h, tn, 1); \
+            tn += h; \
+            steps++; \
+        } \
+\
+        return steps; \
+    }
+
+
+RK4_INTEGRATE_VARIABLE_STEPSIZE(rk4_integrate_variable_stepsize, rk4_t, rk4_integrate, matrix_t)
+RK4_INTEGRATE_VARIABLE_STEPSIZE(rk4_complex_integrate_variable_stepsize, rk4_complex_t, rk4_complex_integrate, matrix_complex_t)
 
 
 /** @brief Initialize symplectic Runge Kutta 4th order integration
@@ -178,11 +255,9 @@ void rk4_symplectic_free(rk4_symplectic_t *self)
 
 static inline void interpolate(const double t1, const double t2, const double x[3], const double f[3], double *w1, double *w2)
 {
-    double a0,a1,a2;
-
-    a0 = f[0];
-    a1 = (f[1]-a0)/( x[1]-x[0] );
-    a2 = ( (f[2]-f[0])/(x[2]-x[0]) - a1 )/(x[2]-x[1]);
+    double a0 = f[0];
+    double a1 = (f[1]-a0)/( x[1]-x[0] );
+    double a2 = ( (f[2]-f[0])/(x[2]-x[0]) - a1 )/(x[2]-x[1]);
 
     *w1 = a0 + (t1-x[0])*( a1 + a2*(t1-x[1]) );
     *w2 = a0 + (t2-x[0])*( a1 + a2*(t2-x[1]) );
@@ -300,11 +375,11 @@ int rk4_symplectic_integrate(rk4_symplectic_t *self, matrix_t *yn, double t, dou
         else
             h = self->adapt(self->f, yn, t, self->args);
 
-       if((t-tn) < h)
-       {
-           h = t-tn;
-           bye = 1;
-       }
+        if((t-tn) < h)
+        {
+            h = t-tn;
+            bye = 1;
+        }
 
         /* determine starting approximations */
         {
