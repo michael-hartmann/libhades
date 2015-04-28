5721de55b092484692ba4e1a5f5dd0fb8fd68f3e
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-04-28 12:40:33 +0200

    wip

--- src/libhades/odeint.h
+++ src/libhades/odeint.h
@@ -14,9 +14,40 @@ typedef struct {
     matrix_t *workspace[8];
 } rk4_symplectic_t;
 
+typedef struct {
+    void (*f)(matrix_t *ft, matrix_t *y, double t, void *args);
+    int maxiter;
+    double epsilon;
+    void *args;
+    double (*adapt)(void (*f)(matrix_t *, matrix_t *, double, void *), matrix_t *y, double t, void *args);
 
-int rk4        (void (*f)(double t, matrix_t         *y, matrix_t         *ft, void *args), matrix_t         *yn, double t0, double t, double h, void *args);
-int rk4_complex(void (*f)(double t, matrix_complex_t *y, matrix_complex_t *ft, void *args), matrix_complex_t *yn, double t0, double t, double h, void *args);
+    /* workspace */
+    matrix_t *workspace[5];
+} rk4_t;
+
+typedef struct {
+    void (*f)(matrix_complex_t *ft, matrix_complex_t *y, double t, void *args);
+    int maxiter;
+    double epsilon;
+    void *args;
+    double (*adapt)(void (*f)(matrix_complex_t *, matrix_complex_t *, double, void *), matrix_complex_t *y, double t, void *args);
+
+    /* workspace */
+    matrix_complex_t *workspace[5];
+} rk4_complex_t;
+
+int rk4_init        (rk4_t         *self, void (*f)(matrix_t *, matrix_t *, double, void *),                 int dim, void *args);
+int rk4_complex_init(rk4_complex_t *self, void (*f)(matrix_complex_t *, matrix_complex_t *, double, void *), int dim, void *args);
+
+void rk4_free        (rk4_t         *self);
+void rk4_complex_free(rk4_complex_t *self);
+
+int rk4_integrate        (rk4_t         *self, matrix_t         *yn, double t, double t0, int steps);
+int rk4_complex_integrate(rk4_complex_t *self, matrix_complex_t *yn, double t, double t0, int steps);
+
+int rk4_integrate_variable_stepsize        (rk4_t         *self, matrix_t         *yn, double t, double t0, double (*adapt)(void (*f)(matrix_t         *, matrix_t         *, double, void *), matrix_t         *y, double t, void *args));
+int rk4_complex_integrate_variable_stepsize(rk4_complex_t *self, matrix_complex_t *yn, double t, double t0, double (*adapt)(void (*f)(matrix_complex_t *, matrix_complex_t *, double, void *), matrix_complex_t *y, double t, void *args));
+
 
 int rk4_symplectic_init(rk4_symplectic_t *self, void (*f)(matrix_t *, matrix_t *, double, void *), int dim, void *args);
 int rk4_symplectic_init_full(rk4_symplectic_t *self, void (*f)(matrix_t *, matrix_t *, double, void *), int dim, void *args, int maxiter, double epsilon, double (adapt)(void (*f)(matrix_t *, matrix_t *, double, void *), matrix_t *y, double t, void *args));
