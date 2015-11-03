ffe90fb1b4fba70237348363b66a148b902e89e8
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-11-03 08:21:50 +0100

    remove

--- src/libhades/expm.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBHADES_EXPM_H
-#define LIBHADES_EXPM_H
-
-#include <libhades.h>
-
-int matrix_complex_expm(matrix_complex_t *A);
-
-#endif
--- src/libhades/optimize.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIBHADES_OPTIMIZE_H
-#define LIBHADES_OPTIMIZE_H
-
-int newton_mdim(void (*f)(matrix_t *, matrix_t *, void *), void (*Jacobian)(matrix_t *, matrix_t *, void *), matrix_t *xn, double eps, int maxiter, void *args);
-
-#endif
