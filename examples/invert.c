5cf197c640675f7b3a865de20e4d0e97a4c1c747
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-11-03 08:06:56 +0100

    add comments

--- examples/invert.c
+++ examples/invert.c
@@ -3,8 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+    /* allocate a real 2x2 matrix */
     matrix_t *M = matrix_alloc(2,2);
 
+    /* initialize matrix */
     matrix_set(M, 0,0, 1);
     matrix_set(M, 0,1, 2);
     matrix_set(M, 1,0, 3);
@@ -13,10 +15,14 @@ int main(int argc, char *argv[])
     printf("Inverse of\n");
     matrix_fprintf(stdout, M, "%+4g", "  ", "\n");
 
+    /* invert matrix */
     matrix_invert(M);
 
     printf("is:\n");
     matrix_fprintf(stdout, M, "%+4g", "  ", "\n");
+
+    /* free matrix */
+    matrix_free(M);
      
     return 0;
 }
